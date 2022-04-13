6e0dbaacc7df1c152a15d90bffa0fdb55f08f552
Gabriel Mota <gabriel.mota@gpnet.com.br>
2022-04-13 18:37:23 -0300

    Inicio ( barra e logo bugado )

---
+++ js/app.js
@@ -0,0 +1,31 @@
+/* --------------- Grab elements from DOM --------------- */
+const header = document.querySelector("header")
+
+/* --------------- Sticky Navbar --------------- */
+
+function stickyNavbar(){
+    header.classList.toggle("scrolled", window.pageYOffset > 0);
+}
+
+stickyNavbar
+
+window.addEventListener("scroll", stickyNavbar)
+
+
+/* --------------- Reveal Animation --------------- */
+
+/* --------------- Skills Progress Bar Animation --------------- */
+
+/* --------------- Services Counter Animation --------------- */
+
+/* --------------- Portfolio Filter Animation --------------- */
+
+/* --------------- Modal Pop Up Animation Animation --------------- */
+
+/* --------------- Modal Pop Up Animation Animation --------------- */
+
+/* --------------- Change Active Link On Scroll --------------- */
+
+/* --------------- Change Page Theme --------------- */
+
+/* --------------- Open & Close Navbar Menu --------------- */
