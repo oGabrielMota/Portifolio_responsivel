545d50a099a554a4383c879bb22d811bb9a78226
Gabriel Mota <gabriel.mota@gpnet.com.br>
2022-05-12 20:55:44 -0300

    Imprementação de Sobre e Skills

--- js/app.js
+++ js/app.js
@@ -1,20 +1,77 @@
 /* --------------- Grab elements from DOM --------------- */
-const header = document.querySelector("header")
+
+const cabeça = document.querySelector("header");
+
+const skill_primeira = document.querySelector(".skill:first-child");
+const contadors = document.querySelectorAll(".contador span");
+const barra =  document.querySelectorAll(".skills svg circle");
+
+window.addEventListener("scroll", ()=>{
+    skillsConters();
+})
 
 /* --------------- Sticky Navbar --------------- */
 
 function stickyNavbar(){
-    header.classList.toggle("scrolled", window.pageYOffset > 0);
+    cabeça.classList.toggle("rolada", window.scrollY > 0);
 }
 
-stickyNavbar
+stickyNavbar();
 
-window.addEventListener("scroll", stickyNavbar)
+window.addEventListener("scroll", stickyNavbar);
 
 
 /* --------------- Reveal Animation --------------- */
 
+let rolagem = ScrollReveal({
+    duration: 2000,
+    distance: "60px",
+});
+
+rolagem.reveal(".showcase-info", {delay: 500});
+rolagem.reveal(".showcase-image", {origin: "top", delay: 600});
+
+
 /* --------------- Skills Progress Bar Animation --------------- */
+
+
+
+function hasRached(el){
+    let topPosition = el.getBoundingClientRect().top;
+
+    if(window.innerHeight >= topPosition + el.offsetHeight) return true;
+    return false;
+}
+
+
+function updateContador(num, maxNum){
+    let currentNum = +num.innerText;
+
+    if(currentNum < maxNum){
+        num.innerText = currentNum + 1;
+        setTimeout(()=>{
+            updateContador(num, maxNum)
+        }, 12 )
+    }
+}
+
+function skillsConters(){
+    if(!hasRached(skill_primeira)) return;
+
+    contadors.forEach((contador, i) => {
+        let target = +contador.dataset.target;
+        let strokeValue = 427 - 427 *(target / 100);
+
+        barra[i].style.setProperty("--target", strokeValue);
+
+        setTimeout(() => {
+            updateContador(contador, target);
+        },400);
+    })
+    
+    barra.forEach((p) => (p.style.animation = "progress 2s ease-in-out forwards"))
+
+}
 
 /* --------------- Services Counter Animation --------------- */
 
